a9e7f10563a05740dfa02193383e5ba638b8497e
shanys736 <s.segnoungwekem@ecoles-epsi.net>
2025-04-07 10:40:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,18 @@
 from flask import Flask
 from flask import render_template
-from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
+from flask import json
 
+app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
     pyramide = '<pre>'
-    for i in range(1,valeur+1):
-            espaces=''*(valeur-i)
-            chiffres =".join(str(j)for j in range(1,i+1))+
-            ".join(str(j) for j in range(i-1,0,-1))
-            pyramide +=espaces+chiffres+'\n'
-            pyramide +='</pre>'
+    for i in range(1, valeur + 1):
+        espaces = ' ' * (valeur - i)
+        chiffres = ''.join(str(j) for j in range(1, i + 1)) + ''.join(str(j) for j in range(i - 1, 0, -1))
+        pyramide += espaces + chiffres + '\n'
+    pyramide += '</pre>'
     return pyramide
 
-
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
