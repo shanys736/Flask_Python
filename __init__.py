79766d9b382a20c19f0d73a5b6d01b918fed0ac0
shanys736 <s.segnoungwekem@ecoles-epsi.net>
2025-04-07 10:31:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ def exercice(valeur):
     for i in range(1,valeur+1):
             espaces=''*(valeur-i)
             chiffres=".join(str(j)for j in range(1,i+1))+
-            ".join(str(j) for j in range(i,0,-1))
+            ".join(str(j) for j in range(i-1,0,-1))
             pyramide +=espaces+chiffres+'\n'
             pyramide +='</pre>'
     return pyramide
